f34df25010628e4909f37f244322ddb12327dc40
Caroline Oliveira <carolineolv01@gmail.com>
2025-08-09 22:43:42 -0300

    feat: add modulos de database e users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { FinancasModule } from './financas/financas.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PetsModule, FinancasModule, AuthModule],
+  imports: [PetsModule, FinancasModule, AuthModule, DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
