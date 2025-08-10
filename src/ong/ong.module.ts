93a654a88783f2a98c72bdd9d5b293e306660210
Caroline Oliveira <carolineolv01@gmail.com>
2025-08-09 22:47:06 -0300

    feat: alterações nos modulos de ong e pets

--- src/ong/ong.module.ts
+++ src/ong/ong.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ong } from './entities/ong.entity';
+import { OngService } from './services/ong.service';
+import { OngController } from './controllers/ong.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Ong])],
+  providers: [OngService],
+  controllers: [OngController],
+})
 export class OngModule {}
--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PetsController } from './controllers/pets.controller';
+import { PetsService } from './services/pets.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pet } from './entities/pet.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Pet])],
+  controllers: [PetsController],
+  providers: [PetsService],
+})
 export class PetsModule {}
