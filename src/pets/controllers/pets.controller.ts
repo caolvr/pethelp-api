0df08cea0be84205ca65bdcc73076221780877f0
Caroline Oliveira <carolineolv01@gmail.com>
2025-08-11 21:27:42 -0300

    feat: CRUD basico para pets

--- src/pets/controllers/pets.controller.ts
+++ src/pets/controllers/pets.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { PetsService } from '../services/pets.service';
+import { Pet } from '../entities/pet.entity';
 
 @Controller('pets')
-export class PetsController {}
+export class PetsController {
+  constructor(private readonly petsService: PetsService) {}
+
+  @Get()
+  findAll() {
+    return this.petsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.petsService.findOne(id);
+  }
+
+  @Post() create(@Body() pet: Pet) {
+    return this.petsService.create(pet);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.petsService.remove(id);
+  }
+}
