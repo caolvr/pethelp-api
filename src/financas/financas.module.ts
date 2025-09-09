99daa8fbef617a73e75b64c6130219d36d75e233
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-08 21:24:33 -0300

    ajuste modulo finanças

--- src/financas/financas.module.ts
+++ src/financas/financas.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { Lancamento } from './entities/lancamento.entity';
+import { CategoriaLancamento } from './entities/categoria-lancamento.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LancamentosController } from './controllers/lancamento.controller';
+import { CategoriaController } from './controllers/categoria-lancamento.controller';
+import { FinancasService } from './services/financas.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Lancamento, CategoriaLancamento])],
+  controllers: [LancamentosController, CategoriaController],
+  providers: [FinancasService],
+})
 export class FinancasModule {}
