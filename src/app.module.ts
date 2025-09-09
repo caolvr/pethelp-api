d367d654ee89d7fb0ea0d20582a3bab66cd5cc63
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-08 21:22:13 -0300

    add modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { FinancasModule } from './financas/financas.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { OngModule } from './ong/ong.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [PetsModule, FinancasModule, AuthModule, DatabaseModule, UsersModule],
+  imports: [
+    PetsModule,
+    OngModule,
+    FinancasModule,
+    AuthModule,
+    DatabaseModule,
+    UsersModule,
+    EmailModule,
+    FinancasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
