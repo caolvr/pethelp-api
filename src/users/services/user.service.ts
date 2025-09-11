5912a3f188d5bc87b122b466d05058947e2d6764
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-10 21:25:04 -0300

    feat: add create e update no user service

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/CreateUserDto';
+import { UpdateUserDto } from '../dtos/UpdateUserDto';
 
 @Injectable()
 export class UserService {
@@ -11,5 +13,15 @@ export class UserService {
 
   findEmail(email: string): Promise<User | null> {
     return this.userRepository.findOne({ where: { email } });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser);
+  }
+
+  async update(updateUserDto: UpdateUserDto): Promise<User | null> {
+    await this.userRepository.update(updateUserDto.id, updateUserDto);
+    return this.userRepository.findOne({ where: { id: updateUserDto.id } });
   }
 }
