f9be205a095ec48dcbb2200169a3172d60a93447
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:46:37 -0300

    fix: add filtro de ong nos metodos

--- src/pets/services/pets.service.ts
+++ src/pets/services/pets.service.ts
@@ -8,26 +8,38 @@ import { CreatePetDto } from '../dtos/CreatePetDto';
 export class PetsService {
   constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
 
-  findAll(): Promise<Pet[]> {
-    return this.petsRepository.find();
+  findAll(ongId: string): Promise<Pet[]> {
+    return this.petsRepository.find({ where: { ong: { id: ongId } } });
   }
 
-  findOne(id: string): Promise<Pet | null> {
-    return this.petsRepository.findOneBy({ id: id });
+  findAllAdocao(): Promise<Pet[]> {
+    return this.petsRepository.find({
+      where: { status: 'disponivel' },
+    });
   }
 
-  async remove(id: string): Promise<void> {
-    await this.petsRepository.delete(id);
+  findOne(id: string, ongId: string): Promise<Pet | null> {
+    return this.petsRepository.findOneBy({ id: id, ong: { id: ongId } });
   }
 
-  async create(createPetDto: CreatePetDto): Promise<Pet> {
-    const pet = this.petsRepository.create(createPetDto);
+  async remove(id: string, ongId: string): Promise<void> {
+    await this.petsRepository.delete({ id: id, ong: { id: ongId } });
+  }
+
+  async create(createPetDto: CreatePetDto, ongId: string): Promise<Pet> {
+    const pet = this.petsRepository.create({
+      ...createPetDto,
+      ong: { id: ongId },
+    });
     return await this.petsRepository.save(pet);
   }
 
-  async update(id: string, pet: Pet): Promise<Pet> {
-    await this.petsRepository.update(id, pet);
-    const updatedPet = await this.petsRepository.findOneBy({ id });
+  async update(id: string, pet: Pet, ongId: string): Promise<Pet> {
+    await this.petsRepository.update({ id: id, ong: { id: ongId } }, pet);
+    const updatedPet = await this.petsRepository.findOneBy({
+      id: id,
+      ong: { id: ongId },
+    });
     if (!updatedPet) {
       throw new Error(`Pet com o id ${id} não encontrado após update.`);
     }
