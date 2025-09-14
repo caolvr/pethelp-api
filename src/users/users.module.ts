7e167668f5898e00f8aeb09108a481c26fcf1c02
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:47:07 -0300

    feat: ajuste imports e providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './controllers/users.controller';
+import { User } from './entities/user.entity';
+import { UserService } from './services/user.service';
 
 @Module({
-  controllers: [UsersController]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UsersModule {}
