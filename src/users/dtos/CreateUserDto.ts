8c219a7196e244dc815890f2f2b6ee5e6bf2a0a6
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:47:23 -0300

    fix: add readonly nos atributos do dto

--- src/users/dtos/CreateUserDto.ts
+++ src/users/dtos/CreateUserDto.ts
@@ -4,18 +4,15 @@ import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  nome: string;
+  readonly nome: string;
 
   @IsEmail()
-  email: string;
+  readonly email: string;
 
   @IsString()
   @IsNotEmpty()
-  cpf: string;
+  readonly cpf: string;
 
   @IsString()
-  celular: string;
-
-  @IsString()
-  senha: string;
+  readonly celular: string;
 }
