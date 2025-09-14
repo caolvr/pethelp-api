1be85b7cdc7d623923c25cfd221edd177da6aea3
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:38:51 -0300

    fix: imports e providers

--- src/ong/ong.module.ts
+++ src/ong/ong.module.ts
@@ -5,10 +5,16 @@ import { OngService } from './services/ong.service';
 import { OngController } from './controllers/ong.controller';
 import { User } from 'src/users/entities/user.entity';
 import { EmailService } from 'src/email/services/email.service';
+import { UserService } from 'src/users/services/user.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ong]), TypeOrmModule.forFeature([User])],
-  providers: [OngService, EmailService],
+  imports: [
+    TypeOrmModule.forFeature([Ong]),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+  ],
+  providers: [OngService, EmailService, UserService],
   controllers: [OngController],
 })
 export class OngModule {}
