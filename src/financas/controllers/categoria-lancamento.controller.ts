217c81ab004750e1f856f020cd1c70ed5ffc4231
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:36:37 -0300

    feat: add guards

--- src/financas/controllers/categoria-lancamento.controller.ts
+++ src/financas/controllers/categoria-lancamento.controller.ts
@@ -6,35 +6,52 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { FinancasService } from '../services/financas.service';
 import { CreateCategoriaDto } from '../dtos/CreateCategoriaDto';
 import { CategoriaLancamento } from '../entities/categoria-lancamento.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Controller('categoria-financas')
 export class CategoriaController {
   constructor(private readonly financasService: FinancasService) {}
+
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAllCategories() {
-    return this.financasService.findAllCategories();
+  findAllCategories(@CurrentUser('ongId') ongId: string) {
+    return this.financasService.findAllCategories(ongId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOneCategory(@Param('id') id: string) {
-    return this.financasService.findOneCategory(id);
+  findOneCategory(
+    @Param('id') id: string,
+    @CurrentUser('ongId') ongId: string,
+  ) {
+    return this.financasService.findOneCategory(id, ongId);
   }
 
-  @Post() createCategory(@Body() categoria: CreateCategoriaDto) {
-    return this.financasService.createCategory(categoria);
+  @Post()
+  createCategory(
+    @Body() categoria: CreateCategoriaDto,
+    @CurrentUser('ongId') ongId: string,
+  ) {
+    return this.financasService.createCategory(categoria, ongId);
   }
 
   @Delete(':id')
-  removeCategory(@Param('id') id: string) {
-    return this.financasService.removeCategory(id);
+  removeCategory(@Param('id') id: string, @CurrentUser('ongId') ongId: string) {
+    return this.financasService.removeCategory(id, ongId);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() categoria: CategoriaLancamento) {
-    return this.financasService.updateCategory(id, categoria);
+  update(
+    @Param('id') id: string,
+    @Body() categoria: CategoriaLancamento,
+    @CurrentUser('ongId') ongId: string,
+  ) {
+    return this.financasService.updateCategory(id, categoria, ongId);
   }
 }
--- src/financas/controllers/lancamento.controller.ts
+++ src/financas/controllers/lancamento.controller.ts
@@ -6,37 +6,53 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { FinancasService } from '../services/financas.service';
 import { CreateLancamentoDto } from '../dtos/CreateLancamentoDto';
 import { Lancamento } from '../entities/lancamento.entity';
-import { CreateCategoriaDto } from '../dtos/CreateCategoriaDto';
+import { TokenValidationGuard } from 'src/auth/guards/token-validation.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
+@UseGuards(TokenValidationGuard)
 @Controller('financas')
 export class LancamentosController {
   constructor(private readonly financasService: FinancasService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.financasService.findAll();
+  findAll(@CurrentUser('ongId') ongId: string) {
+    return this.financasService.findAll(ongId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.financasService.findOne(id);
+  findOne(@Param('id') id: string, @CurrentUser('ongId') ongId: string) {
+    return this.financasService.findOne(id, ongId);
   }
 
-  @Post() create(@Body() lancamento: CreateLancamentoDto) {
-    return this.financasService.create(lancamento);
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(
+    @CurrentUser('ongId') ongId: string,
+    @Body() lancamento: CreateLancamentoDto,
+  ) {
+    return this.financasService.create(lancamento, ongId);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.financasService.remove(id);
+  remove(@Param('id') id: string, @CurrentUser('ongId') ongId: string) {
+    return this.financasService.remove(id, ongId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
-  update(@Param('id') id: string, @Body() lancamento: Lancamento) {
-    return this.financasService.update(id, lancamento);
+  update(
+    @Param('id') id: string,
+    @Body() lancamento: Lancamento,
+    @CurrentUser('ongId') ongId: string,
+  ) {
+    return this.financasService.update(id, lancamento, ongId);
   }
 }
