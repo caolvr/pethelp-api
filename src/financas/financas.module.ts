47c156d3114fa2fc3f6abebf2946714444e3e48a
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-14 13:34:58 -0300

    fix: ajuste de imports

--- src/financas/financas.module.ts
+++ src/financas/financas.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LancamentosController } from './controllers/lancamento.controller';
 import { CategoriaController } from './controllers/categoria-lancamento.controller';
 import { FinancasService } from './services/financas.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lancamento, CategoriaLancamento])],
+  imports: [
+    TypeOrmModule.forFeature([Lancamento, CategoriaLancamento]),
+    AuthModule,
+  ],
   controllers: [LancamentosController, CategoriaController],
   providers: [FinancasService],
 })
