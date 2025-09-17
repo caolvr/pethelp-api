6ae01ec3a18eb5bfbd63983ba7673a837788fd0e
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-17 18:27:21 -0300

    add cookie parser e headers de auth

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const corsOptions = {
     origin: 'http://localhost:3001', // Permite todas as origens
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   };
 
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors(corsOptions);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,6 +21,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
