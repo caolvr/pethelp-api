52d0a636dd6cf932a9a3a03a1849db8b4bb9034d
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-17 18:35:31 -0300

    melhoria para utilizar transaction na criação de ong e usuário

--- src/ong/services/ong.service.ts
+++ src/ong/services/ong.service.ts
@@ -4,12 +4,15 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { Ong } from '../entities/ong.entity';
 import { CreateOngDto } from '../dtos/CreateOngDto';
 import { User } from 'src/users/entities/user.entity';
 import { EmailService } from 'src/email/services/email.service';
 import { UserService } from 'src/users/services/user.service';
+import * as bcrypt from 'bcryptjs';
+import * as crypto from 'crypto';
+import { PasswordResetToken } from 'src/auth/entities/password-reset-tokens.entity';
 
 @Injectable()
 export class OngService {
@@ -19,6 +22,7 @@ export class OngService {
     private readonly userRepository: Repository<User>,
     private readonly emailService: EmailService,
     private readonly userService: UserService,
+    private readonly dataSource: DataSource,
   ) {}
 
   findAll(): Promise<Ong[]> {
@@ -46,30 +50,74 @@ export class OngService {
   }
 
   async create(createOngDto: CreateOngDto): Promise<Ong> {
+    console.log('Criando ONG:', createOngDto);
     const { responsavel, ...ongData } = createOngDto;
 
-    const ong = this.ongsRepository.create(ongData);
-    await this.ongsRepository.save(ong);
-
     try {
-      await this.userService.create({
-        ...responsavel,
-      });
-    } catch (e) {
-      if (e instanceof ConflictException) {
-        throw new ConflictException(e.message);
-      }
-    }
+      const { ong, user, token } = await this.dataSource.transaction(
+        async (manager) => {
+          const ongRepo = manager.getRepository(Ong);
+          const userRepo = manager.getRepository(User);
+          const prtRepo = manager.getRepository(PasswordResetToken);
 
-    try {
-      await this.emailService.sendMail(
-        responsavel.email,
-        'Bem-vindo à Plataforma PetHelp',
-        `Olá ${responsavel.nome},\n\nSua ONG foi registrada com sucesso na plataforma PetHelp.\n\nDetalhes de login:\nEmail: ${responsavel.email}\nSenha: 123546\n\nPor favor, altere sua senha após o primeiro login.\n\nAtenciosamente,\nEquipe PetHelp`,
+          const existingUser = await userRepo.findOne({
+            where: { email: responsavel.email },
+          });
+          if (existingUser) {
+            throw new ConflictException('E-mail do responsável já cadastrado.');
+          }
+
+          const ong = ongRepo.create(ongData);
+          await ongRepo.save(ong);
+
+          const tempPassword = crypto.randomBytes(16).toString('hex'); // 32 chars
+          const passwordHash = await bcrypt.hash(tempPassword, 10);
+
+          const user = userRepo.create({
+            ...responsavel,
+            senha: passwordHash,
+            is_admin: true,
+            ong,
+          });
+          await userRepo.save(user);
+
+          const token = crypto.randomBytes(32).toString('hex'); // 64 chars
+          const tokenHash = crypto
+            .createHash('sha256')
+            .update(token)
+            .digest('hex');
+          const expires_at = new Date(Date.now() + 48 * 60 * 60 * 1000); // 48h
+
+          await prtRepo.save(
+            prtRepo.create({
+              user,
+              tokenHash,
+              expires_at,
+            }),
+          );
+
+          return { ong, user, token };
+        },
       );
-    } catch (error) {}
 
-    return ong;
+      try {
+        await this.emailService.sendMail(
+          responsavel.email,
+          'Bem-vindo à Plataforma PetHelp',
+          `Olá ${responsavel.nome},\n\nSua ONG foi registrada com sucesso na plataforma PetHelp.\nCrie uma senha para sua conta: http://localhost:3001/login/create-password?token=${token}\nEquipe PetHelp`,
+        );
+      } catch (error) {
+        console.error('Erro ao enviar e-mail ao responsável:', error);
+      }
+
+      return ong;
+    } catch (e: any) {
+      if (e instanceof ConflictException) {
+        throw e;
+      }
+
+      throw e;
+    }
   }
 
   async update(id: string, pet: Ong): Promise<Ong> {
