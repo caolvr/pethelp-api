9f20201a332ed9c28eaf0c93a4c3314a00ae82d7
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-21 14:35:54 -0300

    feat: add controller para users

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateUserDto } from '../dtos/CreateUserDto';
+import { UserService } from '../services/user.service';
+import { UpdateUserDto } from '../dtos/UpdateUserDto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findById(@CurrentUser('ongId') ongId: string, @Param('id') userId: string) {
+    return this.userService.findOneById(userId, ongId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() user: UpdateUserDto,
+    @CurrentUser('ongId') ongId: string,
+  ) {
+    return this.userService.update(user, ongId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  findAll(@CurrentUser('ongId') ongId: string) {
+    return this.userService.findAll(ongId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@CurrentUser('ongId') ongId: string, @Body() user: CreateUserDto) {
+    return this.userService.create(user, ongId);
+  }
+}
