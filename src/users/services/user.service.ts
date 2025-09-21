4f0b93f3667d5c1a0be88edd469bb502573ee1ca
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-21 14:37:11 -0300

    feat: refatoração de user service para incluir ongId nos métodos find e melhoria da lógica create

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -1,9 +1,11 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos/CreateUserDto';
 import { UpdateUserDto } from '../dtos/UpdateUserDto';
+import * as bcrypt from 'bcryptjs';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UserService {
@@ -11,36 +13,71 @@ export class UserService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
+  findAll(ongId: string): Promise<User[]> {
+    return this.userRepository.find({
+      where: { ong: { id: ongId } },
+      select: {
+        id: true,
+        nome: true,
+        email: true,
+        cpf: true,
+        celular: true,
+        ativo: true,
+        is_admin: true,
+      },
+    });
+  }
+
   findEmail(email: string): Promise<User | null> {
     return this.userRepository.findOne({ where: { email } });
   }
 
-  findOne(email: string): Promise<User | null> {
-    return this.userRepository.findOne({
+  findOne(email: string, manager?: EntityManager): Promise<User | null> {
+    const repo = manager ? manager.getRepository(User) : this.userRepository;
+    return repo.findOne({
       where: { email },
       relations: ['ong'],
     });
   }
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = this.userRepository.create(createUserDto);
-
-    if (newUser.cpf) {
-      const existingUser = await this.userRepository.findOne({
-        where: { cpf: newUser.cpf },
-      });
-      if (existingUser) {
-        throw new ConflictException(
-          'Usuário responsável já cadastrado com este CPF',
-        );
-      }
-    }
-
-    return this.userRepository.save(newUser);
+  findOneById(
+    id: string,
+    ongId: string,
+    manager?: EntityManager,
+  ): Promise<User | null> {
+    const repo = manager ? manager.getRepository(User) : this.userRepository;
+    return repo.findOne({
+      where: { id, ong: { id: ongId } },
+      relations: ['ong'],
+    });
   }
 
-  async update(updateUserDto: UpdateUserDto): Promise<User | null> {
-    await this.userRepository.update(updateUserDto.id, updateUserDto);
+  async create(
+    createUserDto: CreateUserDto,
+    ongId: string,
+    manager?: EntityManager,
+  ): Promise<User> {
+    const repo = manager ? manager.getRepository(User) : this.userRepository;
+    const tempPassword = crypto.randomBytes(16).toString('hex'); // 32 chars
+    const passwordHash = await bcrypt.hash(tempPassword, 10);
+
+    const user = repo.create({
+      ...createUserDto,
+      senha: passwordHash,
+      ong: { id: ongId },
+    });
+
+    return repo.save(user);
+  }
+
+  async update(
+    updateUserDto: UpdateUserDto,
+    ongId?: string,
+  ): Promise<User | null> {
+    await this.userRepository.update(updateUserDto.id, {
+      ...updateUserDto,
+      ong: { id: ongId },
+    });
     return this.userRepository.findOne({ where: { id: updateUserDto.id } });
   }
 }
