861bdcfa5ee31ffdb88177ebb2c4407f7de86412
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-21 14:27:25 -0300

    add UpdateLancamentoDto

---
+++ src/financas/dtos/UpdateLancamentoDto.ts
@@ -0,0 +1,46 @@
+import {
+  IsString,
+  IsInt,
+  MinLength,
+  Min,
+  IsOptional,
+  IsEnum,
+  IsUUID,
+  IsUrl,
+  IsDate,
+  IsDecimal,
+  IsNumber,
+  IsBoolean,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class UpdateLancamentoDto {
+  @IsString()
+  readonly data?: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly valor?: number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly descricao?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly data_vencimento?: string;
+
+  @IsBoolean()
+  readonly pago?: boolean;
+
+  @IsOptional()
+  @IsString()
+  readonly data_pagamento?: string;
+
+  @IsOptional()
+  @IsString()
+  readonly observacoes?: string;
+
+  @IsUUID()
+  readonly categoria_id?: string;
+}
