eb035b7a4940b7edeb5dd04c3688c877d00a470c
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-22 01:03:54 -0300

    ajuste de porta smtp

--- src/email/services/email.service.ts
+++ src/email/services/email.service.ts
@@ -8,8 +8,8 @@ export class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
