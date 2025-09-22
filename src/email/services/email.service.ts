27cfe699be0befb92e90fa6597b52d5f08ab1833
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-22 01:21:14 -0300

    add sendgrid

--- src/email/services/email.service.ts
+++ src/email/services/email.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 
+import * as sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+
 @Injectable()
 export class EmailService {
   private transporter;
 
   constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+    // this.transporter = nodemailer.createTransport({
+    //   host: process.env.EMAIL_HOST,
+    //   port: 465,
+    //   secure: true,
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
   }
 
   async sendMail(to: string, subject: string, text: string) {
-    const info = await this.transporter.sendMail({
-      from: '"Minha ONG" <carolineolv01@gmail.com>',
+    const info = await sgMail.send({
+      from: '"PETHELP" <carolineolv01@gmail.com>',
       to,
       subject,
       text,
     });
 
-    console.log('E-mail enviado: %s', info.messageId);
+    console.log('E-mail enviado: %s', info[0].headers['x-message-id']);
   }
 
   async sendForgotPasswordMail(to: string, token: string) {
