ff40ec7283ef8749d0c5c61d2518c2b8c87e4a2f
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-23 23:41:30 -0300

    add smtp do gmail novamente

--- src/email/services/email.service.ts
+++ src/email/services/email.service.ts
@@ -11,47 +11,47 @@ export class EmailService {
   private transporter;
 
   constructor() {
-    // this.transporter = nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: 465,
-    //   secure: true,
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASSWORD,
-    //   },
-    // });
+    this.transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   }
 
   async sendMail(to: string, subject: string, text: string) {
-    const info = await sgMail.send({
-      from: '"PETHELP" <carolineolv01@gmail.com>',
+    const info = await this.transporter.sendMail({
+      from: '"Minha ONG" <carolineolv01@gmail.com>',
       to,
       subject,
       text,
     });
 
-    console.log('E-mail enviado: %s', info[0].headers['x-message-id']);
+    console.log('E-mail enviado: %s', info.messageId);
   }
 
   async sendForgotPasswordMail(to: string, token: string) {
-    const info = await sgMail.send({
+    const info = await this.transporter.sendMail({
       from: '"PETHELP" <carolineolv01@gmail.com>',
       to,
       subject: 'Recuperação de Senha - PetHelp',
-      text: `Crie uma nova senha usando o seguinte link: https://pethelp-web.vercel.app/login/create-password?token=${token}\nEquipe PetHelp`,
+      text: `Crie uma nova senha usando o seguinte link: http://localhost:3001/login/create-password?token=${token}\nEquipe PetHelp`,
     });
 
-    console.log('E-mail enviado: %s', info[0].headers['x-message-id']);
+    console.log('E-mail enviado: %s', info.messageId);
   }
 
   async sendCreatePasswordMail(to: string, token: string, nome: string) {
-    const info = await sgMail.send({
+    const info = await this.transporter.sendMail({
       from: '"PetHelp" <carolineolv01@gmail.com>',
       to,
       subject: 'Criação de Senha - PetHelp',
-      text: `Olá ${nome},\n\nSua conta foi criada com sucesso na plataforma PetHelp.\nCrie uma senha de acesso: https://pethelp-web.vercel.app/login/create-password?token=${token}\nEquipe PetHelp`,
+      text: `Olá ${nome},\n\nSua conta foi criada com sucesso na plataforma PetHelp.\nCrie uma senha de acesso: http://localhost:3001/login/create-password?token=${token}\nEquipe PetHelp`,
     });
 
-    console.log('E-mail enviado: %s', info[0].headers['x-message-id']);
+    console.log('E-mail enviado: %s', info.messageId);
   }
 }
