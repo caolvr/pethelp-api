e03d90f5fc22cfbc1b9ae0c0dfb859fc02a840fd
Caroline Oliveira <carolineolv01@gmail.com>
2025-09-30 21:06:31 -0300

    ajuste para printar no console apenas erros no database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -29,7 +29,7 @@ console.log('[DB PATHS]', {
             dropSchema: true,
             entities: ENTITIES,
             synchronize: true,
-            logging: ['query', 'error'],
+            logging: ['error'],
           };
         }
 
